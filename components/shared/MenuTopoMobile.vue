fff39cfd047b45f4f4ae7288ad1f16bfdc33acda
HenriqueMariano25 <henrique.mariano@agnet.com.br>
2025-08-05 23:04:39 -0300

    Cria menu mobile

---
+++ components/shared/MenuTopoMobile.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import {Icon} from '@iconify/vue';
+import {ref} from 'vue';
+
+const opcoesMenu = ref([{
+  nome: 'Home',
+  rota: '/'
+}, {
+  nome: 'Sobre nós',
+  rota: '/sobre_nos'
+}, {
+  nome: 'Atividades',
+  rota: '/atividades'
+}, {
+  nome: 'Obras',
+  rota: '/obras'
+}, {
+  nome: 'Contatos',
+  rota: '/contatos'
+}, {
+  nome: 'Notícias',
+  rota: '/noticias'
+}])
+
+const mostrarOpcoesMenu = ref<boolean>(false);
+
+</script>
+
+<template>
+  <div class="fixed top-0 left-0 z-50 w-[100vw]">
+    <div class="w-full bg-primary p-2 flex justify-between items-center"
+         @click="mostrarOpcoesMenu = !mostrarOpcoesMenu">
+      <div class="flex gap-2 items-center">
+        <img src="../../assets/images/am_ai_text.png" alt="Logo" class="h-[45px] w-fit"/>
+        <img src="../../assets/images/am_ai_icon.png" alt="Icone" class="h-[35px] w-fit"/>
+      </div>
+      <Icon icon="mingcute:menu-fill" width="48" height="48" style="color: #fff" v-if="!mostrarOpcoesMenu"/>
+      <Icon icon="material-symbols:close-rounded" width="48" height="48" style="color: #fff" v-if="mostrarOpcoesMenu"/>
+    </div>
+    <div class="w-full flex flex-col bg-secondary text-2xl opcoes-menu" :class="{'mostrar': mostrarOpcoesMenu}">
+      <div v-for="opcao in opcoesMenu" class="flex justify-center items-center border-b border-white" :key="opcao.nome">
+        <NuxtLink :to="opcao.rota" class="w-full text-center text-white p-2">
+          {{ opcao.nome }}
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.opcoes-menu {
+  background-color: rgb(41 96 144);
+  opacity: 0;
+  height: 0;
+  transition-duration:  0.1s;
+}
+
+.mostrar {
+  opacity: 1 !important;
+  height: 100% !important;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,18 @@
 
 import MenuTopo from "~/components/shared/MenuTopo.vue";
 import Rodape from "~/components/shared/Rodape.vue";
+import MenuTopoMobile from "~/components/shared/MenuTopoMobile.vue";
+
+const {ehMobile} = useEhMobile()
+
+const mostrarPanelMenuLateralMobile = ref<boolean>(false)
+
 </script>
 
 <template>
-  <div>
-    <MenuTopo/>
+  <div class="relative" :class="{'mt-[64px]': ehMobile}">
+    <MenuTopo v-if="!ehMobile"/>
+    <MenuTopoMobile v-if="ehMobile"/>
     <slot/>
     <Rodape/>
   </div>
